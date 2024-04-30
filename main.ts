710edddfdc83691796141d1a1685543597b3f195
huqz <huqz@yinhai.com>
2024-04-30 15:27:16 +0800

    add readme

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import {Plugin} from "obsidian";
+import {Plugin, TFile} from "obsidian";
 
 export default class CrlfPlugin extends Plugin {
     onload() {
         this.registerEvent(this.app.vault.on('modify', async file => {
-            await this.app.vault.process(file, data => data.replace(/(?<!\r)\n/, '\r\n'))
+            await this.app.vault.process(file as TFile, data => data.replace(/(?<!\r)\n/, '\r\n'))
         }))
     }
 }
